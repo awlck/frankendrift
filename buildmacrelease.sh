5b208ff6a69aacddb6eadeb899054ea6d0e6b367
Adrian Welcker <ardi@diepixelecke.de>
2022-03-22 16:31:04 +0100

    Add macOS build script

---
+++ buildmacrelease.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+if [[ $# -eq 0 ]]; then
+	echo "Version argument required!"
+	exit 1
+fi
+
+SRCDIR=${0:a:h}
+TEMPDIR=$(mktemp -d)
+BLDVER=$1
+
+cleanup() {
+	rm -r "$SRCDIR/FrankenDrift.Adrift/bin"
+	rm -r "$SRCDIR/FrankenDrift.Glue/bin"
+	rm -r "$SRCDIR/FrankenDrift.Runner/FrankenDrift.Runner/bin"
+	rm -r "$SRCDIR/FrankenDrift.Runner/FrankenDrift.Runner.Mac/bin"
+}
+
+cd "$SRCDIR/FrankenDrift.Runner/FrankenDrift.Runner.Mac"
+
+cleanup
+/usr/local/share/dotnet/dotnet publish --self-contained -c Release -f net6.0 -r osx.11.0-arm64 -p:PublishReadyToRun=true -p:TieredCompilation=true
+mkdir "$TEMPDIR/arm64"
+cp -r "bin/Release/net6.0/osx.11.0-arm64/publish/FrankenDrift.Runner.Mac.app" "$TEMPDIR/arm64"
+hdiutil create -format UDZO -volname "FrankenDrift $BLDVER" -srcfolder "$TEMPDIR/arm64" "$SRCDIR/frankendrift-$BLDVER.macarm64.dmg"
+
+cleanup
+/usr/local/share/dotnet/x64/dotnet publish --self-contained -c Release -f net6.0 -r osx-x64 -p:PublishReadyToRun=true -p:TieredCompilation=true
+mkdir "$TEMPDIR/x64"
+cp -r "bin/Release/net6.0/osx-x64/publish/FrankenDrift.Runner.Mac.app" "$TEMPDIR/x64"
+hdiutil create -fs 'HFS+' -format UDZO -volname "FrankenDrift $BLDVER" -srcfolder "$TEMPDIR/x64" "$SRCDIR/frankendrift-$BLDVER.macx64.dmg"
+
+rm -r $TEMPDIR
