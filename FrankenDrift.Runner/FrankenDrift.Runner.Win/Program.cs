e84ecd3d146d334ff37b62205c8aefacd62b7b82
Adrian Welcker <ardi@diepixelecke.de>
2022-04-20 17:32:54 +0200

    Enable error reporting on Windows

--- FrankenDrift.Runner/FrankenDrift.Runner.Win/Program.cs
+++ FrankenDrift.Runner/FrankenDrift.Runner.Win/Program.cs
@@ -8,7 +8,14 @@ namespace FrankenDrift.Runner.Win
         [STAThread]
         public static void Main(string[] args)
         {
+            AppDomain current = AppDomain.CurrentDomain;
+            current.UnhandledException += Current_UnhandledException;
             new Application(Eto.Platforms.WinForms).Run(new MainForm());
+        }
+
+        private static void Current_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("An unhandled exception has occurred, and FrankenDrift needs to shut down. Here's all we know:\n\n" + e.ExceptionObject.ToString(), "FrankenDrift Critical Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
         }
     }
 }
