51fbd364c7eaff4ea52daa5cab76e9ffa78483fa
Adrian Welcker <ardi@diepixelecke.de>
2023-01-16 16:19:17 +0100

    Sorry preliminaries of a webrunner

---
+++ FrankenDrift.WebRunner/Core.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace FrankenDrift.WebRunner
+{
+    public partial class Core
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, Browser!");
+        }
+
+        [JSExport]
+        public static string Greeting()
+        {
+            return "Hello from .NET!";
+        }
+
+
+    }
+}
---
+++ FrankenDrift.WebRunner/main.js
@@ -0,0 +1,25 @@
+﻿import { dotnet } from './dotnet.js'
+
+const is_browser = typeof window != "undefined";
+if (!is_browser) throw new Error(`Expected to be running in a browser`);
+
+const { setModuleImports, getAssemblyExports, getConfig } = 
+  await dotnet.create();
+
+setModuleImports("main.js", {
+  window: {
+    location: {
+      href: () => globalThis.window.location.href
+    }
+  }
+});
+
+const config = getConfig();
+const exports = await getAssemblyExports(config.mainAssemblyName);
+console.log(exports);
+const text = exports.FrankenDrift.WebRunner.Core.Greeting();
+console.log(text);
+
+document.getElementById("out").innerHTML = text;
+//await dotnet.run();
+
