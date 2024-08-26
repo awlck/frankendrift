9e2cf88d56a8cd99ee4cb3fec918495fd3b1f3c0
Adrian Welcker <ardi@diepixelecke.de>
2024-08-26 11:40:06 +0200

    Fix mac build script

--- buildmacrelease.sh
+++ buildmacrelease.sh
@@ -18,14 +18,14 @@ cleanup() {
 cd "$SRCDIR/FrankenDrift.Runner/FrankenDrift.Runner.Mac"
 
 cleanup
-/usr/local/share/dotnet/dotnet publish --self-contained -c Release -f net8.0 -r osx-arm64 -p:PublishReadyToRun=true -p:TieredCompilation=true
+/usr/local/share/dotnet/dotnet publish --self-contained -c Release -f net8.0 -r osx-arm64 -p:PublishReadyToRun=true -p:PublishReadyToRunComposite=true -p:TieredCompilation=true -p:Version=$BLDVER -p:IncludeSourceRevisionInInformationalVersion=false
 mkdir "$TEMPDIR/arm64"
 cp -r "bin/Release/net8.0/osx-arm64/publish/FrankenDrift.Runner.Mac.app" "$TEMPDIR/arm64"
 codesign --deep --force -s - "$TEMPDIR/arm64/FrankenDrift.Runner.Mac.app"
 hdiutil create -format UDZO -volname "FrankenDrift $BLDVER" -srcfolder "$TEMPDIR/arm64" "$SRCDIR/frankendrift-$BLDVER-mac.arm64.dmg"
 
 cleanup
-/usr/local/share/dotnet/x64/dotnet publish --self-contained -c Release -f net8.0 -r osx-x64 -p:PublishReadyToRun=true -p:TieredCompilation=true
+/usr/local/share/dotnet/x64/dotnet publish --self-contained -c Release -f net8.0 -r osx-x64 -p:PublishReadyToRun=true -p:PublishReadyToRunComposite=true -p:TieredCompilation=true -p:Version=$BLDVER -p:IncludeSourceRevisionInInformationalVersion=false
 mkdir "$TEMPDIR/x64"
 cp -r "bin/Release/net8.0/osx-x64/publish/FrankenDrift.Runner.Mac.app" "$TEMPDIR/x64"
 codesign --deep --remove-signature "$TEMPDIR/x64/FrankenDrift.Runner.Mac.app"
