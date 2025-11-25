28d1f5e8afee43423b6b7a8abdcb539237bb6cd4
Adrian Welcker <ardi@diepixelecke.de>
2025-11-25 19:53:40 +0100

    Add some error reporting on macOS
    (Because Eto.Mac.CrashReporter doesn't seem to be working.)

--- FrankenDrift.Runner/FrankenDrift.Runner.Mac/Program.cs
+++ FrankenDrift.Runner/FrankenDrift.Runner.Mac/Program.cs
@@ -1,15 +1,24 @@
 ﻿using Eto.Forms;
 using System;
+using System.IO;
 
 namespace FrankenDrift.Runner.Mac
 {
     class Program
     {
+        
         [STAThread]
         public static void Main(string[] args)
         {
 			Eto.Mac.CrashReporter.Attach();
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
             new Application(Eto.Platforms.Mac64).Run(new MainForm());
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            File.WriteAllText("fdcrash.txt", e.ExceptionObject.ToString());
+            Eto.Forms.MessageBox.Show($"An unhandled exception occurred within FrankenDrift: {e.ExceptionObject}", "Unhandled Exception", MessageBoxButtons.OK);
         }
     }
 }
